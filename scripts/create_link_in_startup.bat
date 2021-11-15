af47034c356c04b178ba25cbf201a81d396249ae
Gennady Zykov <zykovgennady@gmail.com>
2021-11-15 20:51:40 +0300

    Add bat file for quick create lnk in the startup folder.

---
+++ scripts/create_link_in_startup.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
+
+echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
+echo sLinkFile = "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\GSimpleWinSleepPreventer.lnk" >> %SCRIPT%
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
+echo oLink.TargetPath = "%~dp0\GSimpleWinSleepPreventer.exe" >> %SCRIPT%
+echo oLink.Arguments = "-s" >> %SCRIPT%
+echo oLink.Save >> %SCRIPT%
+
+cscript /nologo %SCRIPT%
+del %SCRIPT%
